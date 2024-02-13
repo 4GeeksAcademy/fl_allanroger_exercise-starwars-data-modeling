2620d59abb20531c656b6aeb3172b38509abe640
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2024-02-13 20:46:55 +0000

    commitFinal

--- src/models.py
+++ src/models.py
@@ -1,32 +1,39 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, create_engine
+from sqlalchemy.orm import relationship, declarative_base
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    mail = Column(String(100), unique=True, nullable=False)
+    nombre = Column(String(100), nullable=False)
+    contraseña = Column(String(100), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planetas(Base):
+    __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(100), nullable=False)
+    color = Column(String(100), nullable=False) 
+    tamaño = Column(Integer, nullable=False)
 
-    def to_dict(self):
-        return {}
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    color = Column(String(100), nullable=False)
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    usuarioId = Column(Integer, ForeignKey('usuario.id'), nullable=True)
+    planetasId = Column(Integer, ForeignKey('planetas.id'), nullable=True)
+    personajesId = Column(Integer, ForeignKey('personajes.id'), nullable=True)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
